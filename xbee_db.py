2995d3a63ccc1ed704acf853a7926ee6acbacd8e
Daniel Jeon <48095779+danielljeon@users.noreply.github.com>
2024-09-10 23:40:43 -0400

    Improve sender address logging

--- xbee_db.py
+++ xbee_db.py
@@ -24,7 +24,8 @@ class XBeeTransmissionTable(Base):
 
     Columns:
         id: Primary key, auto-increment ID.
-        sender: Sending remote device's 64-bit address.
+        sender_64_hardware: Sending remote device's 64-bit (hardware) address.
+        sender_16_network: Sending remote device's 16-bit (network) address.
         rssi: Signal strength.
         is_broadcast: Boolean condition for direct or broadcast.
         data: Encoded data payload.
@@ -34,7 +35,8 @@ class XBeeTransmissionTable(Base):
     __tablename__ = "xbee_transmissions"
 
     id = Column(Integer, autoincrement=True, primary_key=True)
-    sender = Column(String)
+    sender_64_hardware = Column(String)
+    sender_16_network = Column(String)
     rssi = Column(Integer)
     is_broadcast = Column(Boolean)
     data = Column(String)
@@ -43,7 +45,8 @@ class XBeeTransmissionTable(Base):
     def __str__(self):
         return (
             f"id={self.id}\n"
-            f"sender={self.sender}\n"
+            f"sender_64_hardware={self.sender_64_hardware}\n"
+            f"sender_16_network={self.sender_16_network}\n"
             f"rssi={self.rssi}\n"
             f"is_broadcast={self.is_broadcast}\n"
             f"data={self.data}\n"
@@ -71,7 +74,8 @@ def create_table(table_name: str) -> Table:
         table_name,
         metadata,
         Column("id", Integer, primary_key=True, autoincrement=True),
-        Column("sender", String),
+        Column("sender_64_hardware", String),
+        Column("sender_16_network", String),
         Column("rssi", Integer),
         Column("is_broadcast", Boolean),
         Column("data", String),
@@ -82,8 +86,9 @@ def create_table(table_name: str) -> Table:
 
 
 def new_transmission_record(
-    table: callable(XBeeTransmissionTable),
-    sender: str,
+    table: XBeeTransmissionTable,
+    sender_64_hardware: str,
+    sender_16_network: str,
     rssi: int,
     is_broadcast: bool,
     data: str,
@@ -93,7 +98,8 @@ def new_transmission_record(
 
     Args:
         table: Instance of XBeeTransmissionTable object.
-        sender: Sending remote device's 64-bit address.
+        sender_64_hardware: Sending remote device's 64-bit (hardware) address.
+        sender_16_network: Sending remote device's 16-bit (network) address.
         rssi: Signal strength.
         is_broadcast: Boolean condition for direct or broadcast.
         data: Encoded data payload.
@@ -108,7 +114,8 @@ def new_transmission_record(
 
     # Insert the record using the table object
     insert_stmt = table.insert().values(
-        sender=sender,
+        sender_64_hardware=sender_64_hardware,
+        sender_16_network=sender_16_network,
         rssi=rssi,
         is_broadcast=is_broadcast,
         data=data,
@@ -129,8 +136,9 @@ def log_xbee_transmission(
     """
     new_transmission_record(
         table=table,
-        sender=xbee_message.remote_device.get_64bit_addr(),
-        rssi=xbee_message.remote_device.get_parameter("DB"),
+        sender_64_hardware=str(xbee_message.remote_device.get_64bit_addr()),
+        sender_16_network=str(xbee_message.remote_device.get_16bit_addr()),
+        rssi=int(xbee_message.remote_device.get_parameter("DB")[0]),
         is_broadcast=xbee_message.is_broadcast,
         data=xbee_message.data.decode(),
         timestamp=xbee_message.timestamp,
